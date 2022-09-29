7ef33302bb2c4d0a8cbcac3f620a3de43201481b
Gnilane95 <agathegnilanediouf@gmail.com>
2022-09-29 13:52:29 +0200

    breeze first commit

---
+++ routes/web-copy.php
@@ -0,0 +1,10 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PostController;
+use App\Http\Controllers\Testcontroller;
+// Route::get('/', [Testcontroller::class, 'index'])->name("Home");
+// Route::get('about',[Testcontroller::class, 'about'])->name("A propos");
+Route::get('/',[PostController::class, 'index'])->name("home");
+// Route::get('about',[PostController::class, 'about'])->name("A propos");
+Route::resource('posts', PostController::class);
